57d996defa5f58a99170d3f4c354189f16373792
JaphetSamuel <sagoua1@gmail.com>
2021-09-22 00:09:44 +0000

    chat en cli fix

--- client/target/debug/client.d
+++
@@ -1,1 +0,0 @@
-D:\work\rust\chat\client\target\debug\client.exe: D:\work\rust\chat\client\src\main.rs
--- client/target/debug/deps/client.d
+++
@@ -1,5 +0,0 @@
-D:\work\rust\chat\client\target\debug\deps\client.exe: src\main.rs
-
-D:\work\rust\chat\client\target\debug\deps\client.d: src\main.rs
-
-src\main.rs:
